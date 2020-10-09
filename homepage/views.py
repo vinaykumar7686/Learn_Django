d4284e8d997fc21e4c8cf747c079c9bab6bf8290
Vinay <vinaykumarvk.8866@gmail.com>
2020-10-09 20:18:43 +0530

    added search facility using get method in forms

--- homepage/views.py
+++ homepage/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'homepage/main.html', {})
+    squery = (request.GET) or None
+
+    if not squery or squery['searchquery']=="":
+        squery = None
+
+    if squery is None:
+        return render(request, 'homepage/main.html', {})
+    else:
+        from products.models import Products
+        obj = Products.objects.filter(name__contains = squery['searchquery'])
+        return render(request, 'homepage/searchresults.html', {'res': obj})
