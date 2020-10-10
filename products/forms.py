6d82665f04e8ae96f19eb07bd95bdcb054c0abca
Vinay <vinaykumarvk.8866@gmail.com>
2020-10-10 11:10:23 +0530

    Implemented Model Form

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,28 @@
 from django import forms
+from products.models import Products
 
-class create_product_form(forms.Form):
+'''class create_product_form(forms.Form):
     name = forms.CharField()
     content = forms.CharField()
-    price = forms.IntegerField()
+    price = forms.IntegerField()'''
+
+class create_product_form(forms.ModelForm):
+    class Meta:
+        model = Products        
+        fields = [
+            'name',
+            'content', 
+            'price',
+        ]
+
+    def clean_name(self):
+        val = self.cleaned_data.get('name')
+        if len(val)<2:
+            raise forms.ValidationError("Too Short")
+        return val
+
+    def clean_price(self):
+        val = self.cleaned_data.get('price')
+        if val>1000000:
+            raise forms.ValidationError("To High")
+        return val
--- products/views.py
+++ products/views.py
@@ -27,6 +27,8 @@ def product_specific(request, pk = 1):
         print(e)
         return HttpResponse(f'Product with id {pk} not found!!')
 
+'''
+# Using basic HTML Forms
 def create_products(request):
     print(request.GET, request.POST)
     info = request.POST or None
@@ -41,3 +43,23 @@ def create_products(request):
             from products.models import Products
             Products.objects.create(name = cdata.get('name'), content = cdata.get('content'), price = cdata.get('price'))
     return render(request, 'product_templates/create_products.html' , {})
+    '''
+
+def create_products(request):
+    from .forms import create_product_form
+    myform = create_product_form(request.POST or None)
+
+    if myform.is_valid():
+        
+        myform.save(commit = False)
+
+        # Some Stuff
+
+        myform.save()
+        
+        # redirect to success age or clear the form by "myform = create_product_form()"
+        myform = create_product_form()
+
+    return render(request, 'product_templates/create_products.html', {'form': myform})
+
+    
